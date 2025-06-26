b44384b269f497f9ecbbca70b2457c5f0d28f9c0
christiandoramo <christiandoramo@gmail.com>
2025-06-26 02:14:34 -0300

    chore: nx cloud deploy

--- frontend/src/lib/api/base.ts
+++
@@ -1,4 +0,0 @@
-// src/lib/api/base.ts
-import axios from 'axios';
-
-export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
--- frontend/src/lib/hooks/use-loading.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/hooks/use-loading.ts
-import { create } from 'zustand';
-import { LoadingState } from '../interfaces';
-
-export const useLoading = create<LoadingState>((set) => ({
-  loading: false,
-  setLoading: (value) => set({ loading: value }),
-}));
